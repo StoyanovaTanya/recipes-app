a0583cc87c303379b86f8b50caf316ee8ce4b591
Tanya Stoyanova <tanya100qnova@gmail.com>
2025-12-07 09:43:03 +0200

    Connected recipes to json-server, fixed images paths

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,8 +1,23 @@
-import { recipes } from "../../data/recipes";
 import { Link } from "react-router-dom";
 import styles from "./Recipes.module.css";
+import { useEffect, useState } from "react";
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+  fetch("/recipes")
+    .then(res => {
+      console.log("Fetch response:", res);
+      return res.json();
+    })
+    .then(data => {
+      console.log("Loaded recipes:", data);
+      setRecipes(data);
+    })
+    .catch(err => console.error("Error fetching recipes:", err));
+  }, []);
+  
   return (
     <section style={{ padding: "20px" }}>
       <h2 className={styles.heading}>All Recipes</h2>
